0e05d5b35253aef99b6ce554146fa32ba8dea1ce
unknown <johnnguyen2459@gmail.com>
2015-10-07 15:59:55 -0400

    add new image

---
+++ showimage.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$con=mysqli_connect("localhost","root","","time"); //Change it if required
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM image_table " );
+
+
+while($row = mysqli_fetch_array($result))
+{
+echo '<img src="' . $row['image'] . '" width="200" />';
+echo'<br /><br />';  
+}
+
+
+mysqli_close($con);
+
+?>
